cfad05d7b470eb03dee61832c51e7ec3eead1c98
Mauro Meden <mauro.meden@gmail.com>
2020-02-26 21:38:56 -0500

    Create content for footer
    Start creation of component for work experience.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import data from '../json/data'
+
+const socialMediaList = data.SocialMedias
 
 function Footer() {
     return (
         <footer>
-            <p className="text-center">Insert social media badges here</p>
+            <ul className="navbar-nav">
+                {socialMediaList.map(s => (
+                    <li className="nav-item">
+                        <a href={s} className="nav-link">{s}</a>
+                    </li>
+                ))}
+            </ul>
         </footer>
     )
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import data from '../../json/data'
 
 function Resume() {
     return (
@@ -13,7 +14,7 @@ function Resume() {
                 <section>
                     <div className="row my-3">
                         <div className="col-sm-12">
-                            <article className="row justify-content-center my-3 border-bottom">
+                        <article className="row justify-content-center my-3 border-bottom">
                                 <div className="col-sm-6">
                                     <h3>Mechanic and Personal Assistant</h3>
                                     <h4>Duties and Responsibilities:</h4>
@@ -25,7 +26,20 @@ function Resume() {
                                 </div>
                             </article>
 
-                            <article className="row justify-content-center my-3 border-bottom">
+
+                            {/* <article className="row justify-content-center my-3 border-bottom">
+                                <div className="col-sm-6">
+                                    <h3>Mechanic and Personal Assistant</h3>
+                                    <h4>Duties and Responsibilities:</h4>
+                                    <ul>
+                                        <li>General mechanical duties</li>
+                                        <li>Assisting the Director with day-to-day business administration</li>
+                                        <li>Translating and interpreting between English and Italian</li>
+                                    </ul>
+                                </div>
+                            </article> */}
+
+                            {/* <article className="row justify-content-center my-3 border-bottom">
                                 <div className="col-sm-6">
                                     <h3>Language Teacher</h3>
                                     <h4>Duties and Responsibilities:</h4>
@@ -60,7 +74,7 @@ function Resume() {
                                         <li>...</li>
                                     </ul>
                                 </div>
-                            </article>
+                            </article> */}
                         </div>
                     </div>
                 </section>
