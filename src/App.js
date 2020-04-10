731eedc5d05cccdeb0a1be01aef0918cf5e79a62
maurom88 <mauro.meden@gmail.com>
2020-04-10 15:52:12 -0400

    Create routes for users pages.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import ContactList from './components/pages/ContactList'
 import Home from './components/pages/Home'
 import Portfolio from './components/pages/Portfolio.js'
 import Resume from './components/pages/Resume'
+import Users from './components/pages/Users.js'
+import UsersList from './components/pages/UsersList.js'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
 
 function App() {
   return (
@@ -17,10 +18,12 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/Contact" component={Contact} exact />
-          <Route path="/Contact/list" component={ContactList} exact />
-          <Route path="/Portfolio" component={Portfolio} exact />
-          <Route path="/Resume" component={Resume} exact />
+          <Route path="/contact" component={Contact} exact />
+          <Route path="/contact/list" component={ContactList} exact />
+          <Route path="/portfolio" component={Portfolio} exact />
+          <Route path="/resume" component={Resume} exact />
+          <Route path="/users" component={Users} exact />
+          <Route path="/users/list" component={UsersList} exact />
         </Switch>
         <Footer />
       </div>
