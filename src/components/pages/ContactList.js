f2d8671b0e7100a472fe79cdd551c13b854988d4
maurom88 <mauro.meden@gmail.com>
2020-04-10 14:29:07 -0400

    Fetch form submissions on /contact/list and display results in table.

--- src/components/pages/ContactList.js
+++ src/components/pages/ContactList.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ContactList() {
+
+    const [subs, setSubs] = useState([])
+
+    const url = "http://localhost:9000/contact/list"
+
+    fetch(url, {
+        method: 'GET'
+    })
+        .then(function (response) {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server")
+            }
+            return response.json()
+        })
+        .then(function (data) {
+            setSubs(data)
+        })
+        .catch(err => {
+            console.log('caught it!', err)
+        })
+
     return (
         <div>
-            <p>aaa</p>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone number</th>
+                        <th>Subject</th>
+                        <th>Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {subs.map(sub =>
+                        <tr key={sub.id}>
+                            <td>{sub.Name}</td>
+                            <td>{sub.Email}</td>
+                            <td>{sub.Phone}</td>
+                            <td>{sub.Subject}</td>
+                            <td>{sub.Message}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
         </div>
     )
 }
